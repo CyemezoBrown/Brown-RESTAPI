b3be995956807289f59c26830995b96ab5a32014
CyemezoBrown <cyemezobrown@gmail.com>
2020-12-02 15:26:21 +0200

    removed app.js and added it's codes in server.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
 export default {
     "port": 3005,
-    "mongoUrl": "mongodb://localhost:27017/express-api-blog",
+  //  "mongoUrl": "mongodb://localhost:27017/express-api-blog",
+  //mongodb+srv://brown:test1234@cluster0.7ajvg.mongodb.net/portfolio?retryWrites=true&w=majority
+
     "bodyLimit": "100kb"
 }
--- server.js
+++ server.js
@@ -5,21 +5,52 @@ const cookieParser = require('cookie-parser')
 const posts = require("./routes/posts.routes")
 const userRouter = require("./routes/user.route")
 const jwt = require("jsonwebtoken")
-const User=require("./models/user.model");
+const User =require("./models/user.model");
 const  bcrypt =require("bcrypt");
+const createError = require('http-errors');
+const path = require('path');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
 require("dotenv").config()
 
 let app = express();
 
 const { json } = require("body-parser")
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+
 app.use('/user', userRouter)
 app.use('/api', posts)
 app.use(cookieParser())
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.post('/login', )
  
