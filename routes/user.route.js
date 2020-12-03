f19c159f7f05f423e02f021588c6074204c1a1a0
CyemezoBrown <cyemezobrown@gmail.com>
2020-12-03 16:27:16 +0200

    removed refresh token

--- routes/user.route.js
+++ routes/user.route.js
@@ -66,29 +66,29 @@ router.post('/login', (req, res) => {
         res.status(500).send(err)
     }) 
 });
-router.post('/token', (req, res) => {
-    const { token } = req.body;
+// router.post('/token', (req, res) => {
+//     const { token } = req.body;
 
-    if (!token) {
-        return res.sendStatus(401);
-    }
+//     if (!token) {
+//         return res.sendStatus(401);
+//     }
 
-    if (!refreshTokens.includes(token)) {
-        return res.sendStatus(403);
-    }
+//     if (!refreshTokens.includes(token)) {
+//         return res.sendStatus(403);
+//     }
 
-    jwt.verify(token, refreshTokenSecret, (err, user) => {
-        if (err) {
-            return res.sendStatus(403);
-        }
+//     jwt.verify(token, refreshTokenSecret, (err, user) => {
+//         if (err) {
+//             return res.sendStatus(403);
+//         }
 
-        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+//         const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
 
-        res.json({
-            accessToken
-        });
-    });
-});
+//         res.json({
+//             accessToken
+//         });
+//     });
+// });
 // router.post('/logout', (req, res) => {
 //     const { token } = req.body;
 //     refreshTokens = refreshTokens.filter(token => t !== token);
