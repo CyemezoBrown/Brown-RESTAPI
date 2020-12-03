d97e2a1223a09597e46954697b6f0159f86405db
CyemezoBrown <cyemezobrown@gmail.com>
2020-12-03 09:02:50 +0200

    update middleware

--- controllers/middleware.js
+++ controllers/middleware.js
@@ -1,4 +1,24 @@
-// const jwt = require('jsonwebtoken')
+ const jwt = require('jsonwebtoken')
+ 
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
 
 // module.exports = function (req,res,next){
 //     const accessToken = req.header("auth")
